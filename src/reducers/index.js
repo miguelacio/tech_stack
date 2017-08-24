eda973ea8978bffb4e6a8fed8a9cecab021c99cb
Jesus Miguel Flores <miguelacio@MacBook-Air-de-Jesus.local>
2017-08-24 16:31:20 -0600

    app terminada

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import {combineReducers} from 'redux';
-import LibraryReducer from './LibraryReducer'
-import SelectionReducer from './SelectionReducer'
+import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-    libraries: LibraryReducer,
-    selectedLibraryID: SelectionReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
